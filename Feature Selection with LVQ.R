c94baa234aba59f2338542e1001d4277814a0c1e
ZBaOz <47884046+ZBaOz@users.noreply.github.com>
2023-09-12 10:56:14 +0300

    Feature Selection with LVQ.R

--- Feature Selection with LVQ.R
+++ Feature Selection with LVQ.R
@@ -11,14 +11,12 @@ install.packages("purrr")
 library(purrr)
 
 
-#AAC, CT ve PAAC birleþtirince xlsx formatý için exceed memory hatasý verdi o yüzden csv ye çevirdim. csv formatýný virgülle deðil
-#noktalý virgül ile ayrýlmýþ olarak dönüþtürdü.
-data <- read.csv('C:/Users/zeyne/Desktop/Covid-PPI Proje/Makale1/AAC-CT-PAAC/AAC-CT-PAAA Birlesik.csv',sep=";")
+data <- read.csv('/AAC-CT-PAAA Birlesik.csv',sep=";")
 
 
 x=data[,2:414]
 y=data$label
-#train fonksiyonu için bu vektörün factor olarak çevrilmesini istedi yöntem
+#for train function vectors should be represent as factor. 
 y <- as.factor(y)
 
 control <- trainControl(method="repeatedcv", number=10, repeats=3)
@@ -27,12 +25,10 @@ model <- train(x, y, "lvq", trControl = trainControl(method = "cv"))
 # estimate variable importance
 importance <- varImp(model, scale=FALSE)
 
-#importance bir list verisi. onu data frame e dönüþtürüyor. print(importance) most significant 20 özelliði listeliyor. 
-#Daha fazlasýný listeletmek için data frame e çevirip excel formatýnda yazdýrdým. 
+#'importance' is a list. 'print(importance)' function lists the most significant 20 features
 features=map_df(importance, ~as.data.frame(t(.)))
 
-write.xlsx(features, 'C:/Users/zeyne/Desktop/features.xlsx', sheetName = "Sheet1", 
-           col.names = TRUE, row.names = FALSE, append = FALSE)
+write.xlsx(features, '/features.xlsx', sheetName = "Sheet1", col.names = TRUE, row.names = FALSE, append = FALSE)
 
 
 print(importance)
